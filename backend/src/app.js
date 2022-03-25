b851750f99f57b165b4548274df7b79f800e03a5
Huangyunze <973113137@qq.com>
2022-03-26 03:22:36 +0800

    feat(backend): 添加注册接口，路由，crud boy冲冲冲

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,36 +1,40 @@
-const path = require("path");
+//express_demo.js 文件
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const { create } = require("./utils/createAccount");
 const app = express();
+app.use(require("body-parser").urlencoded({ extended: false }));
 
-// 为应用使用中间件
-// 静态文件中间件
-app.use(express.static(path.join(__dirname, "../build")));
+// 注册接口
+app.post("/register", async (req, res) => {
+    console.log("@@", req.body);
+    const { account, password } = req.body;
+    const rez = await create(account, password);
+    res.send(rez);
+});
+
 // 请求体 parse 中间件，用于 parse json 格式请求体
 app.use(express.json());
 
 // 若无匹配业务路由，则匹配 404 路由，代表访问路径不存在
 app.use(notFound);
-/** 若前面的路由抛错，则封装为错误响应返回
- * 错误响应格式为
- * {
- *   error: message
- * }
- */
+
 app.use(errorHandler);
 
 function notFound(req, res) {
-  res.status(404);
-  res.send({
-    error: "not found",
-  });
+    res.status(404);
+    res.send({
+        error: "not found",
+    });
 }
 
 function errorHandler(err, req, res, next) {
-  // 抛出的错误可以附带 status 字段，代表 http 状态码
-  // 若没有提供，则默认状态码为 500，代表服务器内部错误
-  res.status(err.status || 500);
-  res.send({ error: err.message });
+    // 抛出的错误可以附带 status 字段，代表 http 状态码
+    // 若没有提供，则默认状态码为 500，代表服务器内部错误
+    res.status(err.status || 500);
+    res.send({ error: err.message });
 }
+
 // 导出 Express 对象
 module.exports = app;
---
+++ backend/src/utils/createAccount.js
@@ -0,0 +1,13 @@
+const inspirecloud = require("@byteinspire/inspirecloud-api");
+const create = async (account, password) => {
+    // 使用 inspirecloud.db.table 获取数据表
+    const AccountTable = inspirecloud.db.table("account");
+    // 使用 save 方法新增一条记录
+    const result = await AccountTable.save({ account, password });
+    return {
+        result,
+    };
+};
+module.exports = {
+    create,
+};
