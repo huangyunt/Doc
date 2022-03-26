496c8e9814e3c2d16b4575fa445bbe6f362bd45f
Huangyunze <973113137@qq.com>
2022-03-26 21:25:35 +0800

    chore(backend): update .gitignore

--- backend/src/utils/createAccount.ts
+++ backend/src/utils/createAccount.ts
@@ -2,11 +2,15 @@ const inspirecloud = require("@byteinspire/inspirecloud-api");
 const create = async (account, password) => {
     // 使用 inspirecloud.db.table 获取数据表
     const AccountTable = inspirecloud.db.table("account");
-    // 使用 save 方法新增一条记录
-    const result = await AccountTable.save({ account, password });
-    return {
-        result,
-    };
+    const findOne = await AccountTable.where({ account }).findOne();
+    console.log("** ", findOne);
+    if (!findOne) {
+        // 使用 save 方法新增一条记录
+        const result = await AccountTable.save({ account, password });
+        return { result };
+    } else {
+        return "Account existed";
+    }
 };
 module.exports = {
     create,
